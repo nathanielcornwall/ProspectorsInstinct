7100e85c88ee20e0ddd4982147872b195a52c74d
Nathaniel Cornwall <nate_cornwall-20@hotmail.com>
2026-07-01 18:29:11 -0500

    Implement configuration system and OreScanner framework

---
+++ src/Config/ProspectorsInstinctConfig.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace ProspectorsInstinct.Config;
+
+public class ProspectorsInstinctConfig
+{
+    public bool Enabled { get; set; } = true;
+    public bool RequireProspectingPick { get; set; } = true;
+    public int ScanRadius { get; set; } = 8;
+    public int ScanIntervalMs { get; set; } = 500;
+    public float ParticleDensity { get; set; } = 1.0f;
+
+    public Dictionary<string, bool> DetectOres { get; set; } = new()
+    {
+        ["Native Copper"] = true,
+        ["Cassiterite"] = true,
+        ["Hematite"] = true,
+        ["Limonite"] = true,
+        ["Magnetite"] = true,
+        ["Sphalerite"] = true,
+        ["Bismuthinite"] = true,
+        ["Galena"] = true,
+        ["Malachite"] = true,
+        ["Gold"] = true,
+        ["Silver"] = true,
+        ["Quartz"] = false,
+        ["Coal"] = false,
+        ["Sulfur"] = false,
+        ["Borax"] = false,
+        ["Saltpeter"] = false,
+        ["Uranium"] = true
+    };
+}
---
+++ src/Detection/OreScanner.cs
@@ -0,0 +1,20 @@
+using Vintagestory.API.Common;
+
+namespace ProspectorsInstinct.Detection;
+
+public class OreScanner
+{
+    private readonly ICoreAPI api;
+
+    public OreScanner(ICoreAPI api)
+    {
+        this.api = api;
+
+        api.Logger.Notification("[Prospector's Instinct] OreScanner initialized.");
+    }
+
+    public void Scan()
+    {
+        api.Logger.Notification("[Prospector's Instinct] Scan requested.");
+    }
+}
