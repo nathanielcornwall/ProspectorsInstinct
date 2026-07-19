4acb235d4b8c1db41d33d85e809c8ee3a2ebfafa
Nathaniel Cornwall <nate_cornwall-20@hotmail.com>
2026-07-18 21:25:47 -0500

    Complete runtime ore metadata coverage

--- src/Metadata/OreMetadataProvider.cs
+++ src/Metadata/OreMetadataProvider.cs
@@ -266,6 +266,69 @@ public static class OreMetadataProvider
                 Category = OreCategory.Fuel
             },
 
+            ["anthracite"] = new OreMetadata
+{
+    RuntimeName = "anthracite",
+    DisplayName = "Anthracite",
+    Category = OreCategory.Fuel
+},
+
+["bituminouscoal"] = new OreMetadata
+{
+    RuntimeName = "bituminouscoal",
+    DisplayName = "Bituminous Coal",
+    Category = OreCategory.Fuel
+},
+
+["graphite"] = new OreMetadata
+{
+    RuntimeName = "graphite",
+    DisplayName = "Graphite",
+    Category = OreCategory.Industrial
+},
+
+["fluorite"] = new OreMetadata
+{
+    RuntimeName = "fluorite",
+    DisplayName = "Fluorite",
+    Category = OreCategory.Industrial
+},
+
+["phosphorite"] = new OreMetadata
+{
+    RuntimeName = "phosphorite",
+    DisplayName = "Phosphorite",
+    Category = OreCategory.Industrial
+},
+
+["alum"] = new OreMetadata
+{
+    RuntimeName = "alum",
+    DisplayName = "Alum",
+    Category = OreCategory.Chemical
+},
+
+["kernite"] = new OreMetadata
+{
+    RuntimeName = "kernite",
+    DisplayName = "Kernite",
+    Category = OreCategory.Chemical
+},
+
+["sylvite"] = new OreMetadata
+{
+    RuntimeName = "sylvite",
+    DisplayName = "Sylvite",
+    Category = OreCategory.Chemical
+},
+
+["seabed"] = new OreMetadata
+{
+    RuntimeName = "seabed",
+    DisplayName = "Seabed Deposit",
+    Category = OreCategory.Misc
+},
+
             ["quartz"] = new OreMetadata
             {
                 RuntimeName = "quartz",
@@ -303,6 +366,183 @@ public static class OreMetadataProvider
                 Category = OreCategory.Chemical,
                 EnabledByDefault = false
             }
+
+            ,
+
+            ["berylaquamarine"] = new OreMetadata
+{
+    RuntimeName = "berylaquamarine",
+    DisplayName = "Aquamarine",
+    Category = OreCategory.Gemstone
+},
+
+["berylbixbite"] = new OreMetadata
+{
+    RuntimeName = "berylbixbite",
+    DisplayName = "Bixbite",
+    Category = OreCategory.Gemstone
+},
+
+["corundum"] = new OreMetadata
+{
+    RuntimeName = "corundum",
+    DisplayName = "Corundum",
+    Category = OreCategory.Gemstone
+},
+
+["corundumruby"] = new OreMetadata
+{
+    RuntimeName = "corundumruby",
+    DisplayName = "Ruby",
+    Category = OreCategory.Gemstone
+},
+
+["corundumsapphire"] = new OreMetadata
+{
+    RuntimeName = "corundumsapphire",
+    DisplayName = "Sapphire",
+    Category = OreCategory.Gemstone
+},
+
+["diamond"] = new OreMetadata
+{
+    RuntimeName = "diamond",
+    DisplayName = "Diamond",
+    Category = OreCategory.Gemstone
+},
+
+["emerald"] = new OreMetadata
+{
+    RuntimeName = "emerald",
+    DisplayName = "Emerald",
+    Category = OreCategory.Gemstone
+},
+
+["flint"] = new OreMetadata
+{
+    RuntimeName = "flint",
+    DisplayName = "Flint",
+    Category = OreCategory.Misc
+},
+
+["garnetalmandine"] = new OreMetadata
+{
+    RuntimeName = "garnetalmandine",
+    DisplayName = "Almandine Garnet",
+    Category = OreCategory.Gemstone
+},
+
+["garnetandradite"] = new OreMetadata
+{
+    RuntimeName = "garnetandradite",
+    DisplayName = "Andradite Garnet",
+    Category = OreCategory.Gemstone
+},
+
+["garnetgrossular"] = new OreMetadata
+{
+    RuntimeName = "garnetgrossular",
+    DisplayName = "Grossular Garnet",
+    Category = OreCategory.Gemstone
+},
+
+["garnetpyrope"] = new OreMetadata
+{
+    RuntimeName = "garnetpyrope",
+    DisplayName = "Pyrope Garnet",
+    Category = OreCategory.Gemstone
+},
+
+["garnetspessartine"] = new OreMetadata
+{
+    RuntimeName = "garnetspessartine",
+    DisplayName = "Spessartine Garnet",
+    Category = OreCategory.Gemstone
+},
+
+["garnetuvarovite"] = new OreMetadata
+{
+    RuntimeName = "garnetuvarovite",
+    DisplayName = "Uvarovite Garnet",
+    Category = OreCategory.Gemstone
+},
+
+["lapislazuli"] = new OreMetadata
+{
+    RuntimeName = "lapislazuli",
+    DisplayName = "Lapis Lazuli",
+    Category = OreCategory.Gemstone
+},
+
+["olivine"] = new OreMetadata
+{
+    RuntimeName = "olivine",
+    DisplayName = "Olivine",
+    Category = OreCategory.Gemstone
+},
+
+["olivine_peridot"] = new OreMetadata
+{
+    RuntimeName = "olivine_peridot",
+    DisplayName = "Peridot",
+    Category = OreCategory.Gemstone
+},
+
+["spinelred"] = new OreMetadata
+{
+    RuntimeName = "spinelred",
+    DisplayName = "Red Spinel",
+    Category = OreCategory.Gemstone
+},
+
+["topazamber"] = new OreMetadata
+{
+    RuntimeName = "topazamber",
+    DisplayName = "Amber Topaz",
+    Category = OreCategory.Gemstone
+},
+
+["topazblue"] = new OreMetadata
+{
+    RuntimeName = "topazblue",
+    DisplayName = "Blue Topaz",
+    Category = OreCategory.Gemstone
+},
+
+["topazpink"] = new OreMetadata
+{
+    RuntimeName = "topazpink",
+    DisplayName = "Pink Topaz",
+    Category = OreCategory.Gemstone
+},
+
+["tourmalinerubellite"] = new OreMetadata
+{
+    RuntimeName = "tourmalinerubellite",
+    DisplayName = "Rubellite",
+    Category = OreCategory.Gemstone
+},
+
+["tourmalineschorl"] = new OreMetadata
+{
+    RuntimeName = "tourmalineschorl",
+    DisplayName = "Schorl",
+    Category = OreCategory.Gemstone
+},
+
+["tourmalineverdelite"] = new OreMetadata
+{
+    RuntimeName = "tourmalineverdelite",
+    DisplayName = "Verdelite",
+    Category = OreCategory.Gemstone
+},
+
+["tourmalinewatermelon"] = new OreMetadata
+{
+    RuntimeName = "tourmalinewatermelon",
+    DisplayName = "Watermelon Tourmaline",
+    Category = OreCategory.Gemstone
+},
         };
 
     public static OreMetadata Get(string runtimeName)
