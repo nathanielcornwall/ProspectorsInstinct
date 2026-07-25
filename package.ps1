ea1b75d0500ef58c007320e3683fc26451983d6f
Nathaniel Cornwall <nate_cornwall-20@hotmail.com>
2026-07-25 17:03:14 -0500

    Bump version to 0.8.0

--- package.ps1
+++ package.ps1
@@ -12,7 +12,7 @@ if ($LASTEXITCODE -ne 0)
     exit
 }
 
-$zipName = "ProspectorsInstinct_0.7.1.zip"
+$zipName = "ProspectorsInstinct_0.8.0.zip"
 
 # Delete previous package
 if (Test-Path $zipName)
